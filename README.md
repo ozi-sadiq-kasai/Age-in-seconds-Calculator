adffb0cd03e413758e45960bd3374a83203c10af
ozi <ozisadiqkasai@gmail.com>
2022-09-15 22:20:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# A code that calculates how many seconds a person has lived on planet Earth.
+# The code is placed in a function which enables reuseability.
---
+++ age.py
@@ -0,0 +1,8 @@
+def age_in_seconds_calculator():
+    age = int(input('How old are you (Years)? '))
+    seconds = 31622400
+    age_second = age * seconds
+    print(f'you have lived for {age_second} seconds')
+
+age_in_seconds_calculator()
+
